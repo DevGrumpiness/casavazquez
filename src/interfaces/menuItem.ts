06e344701ade66a77396d6420f9ea123c6502f2f
DevGrumpiness <b.vazquez@outlook.de>
2023-12-17 11:35:56 +0100

    update menuItem type definition

--- src/interfaces/menuItem.ts
+++ src/interfaces/menuItem.ts
@@ -1,8 +1,20 @@
-// define interface for menu item here
-export type TMenuItem = {
+interface MenuItem {
 	id: number;
 	name: string;
 	description: string;
 	price: number;
-	category: "drink" | "food";
-};
+	imageUrl: string | null;
+	available: boolean;
+	allergens?: string[];
+}
+
+type DrinkSize = "standard" | "small" | "medium" | "large"; // Drinks that come only in one size should be "standard"
+
+export interface TMenuItemFood extends MenuItem {
+	label: "veggie" | "vegan" | null;
+}
+
+export interface TMenuItemDrink extends MenuItem {
+	label: "alcoholic" | "hot" | null;
+	availableSizes: DrinkSize[];
+}
