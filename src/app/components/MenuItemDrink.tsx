1f9922f9cfa06be53d76dcdc5d3fad8534d22eae
DevGrumpiness <b.vazquez@outlook.de>
2023-12-22 16:46:08 +0100

    use button instead of div for liste elements

--- src/app/components/MenuItemDrink.tsx
+++ src/app/components/MenuItemDrink.tsx
@@ -14,13 +14,13 @@ export const MenuItemDrink: React.FC<MenuItemDrinkProps> = ({ drink }) => {
 		"https://placehold.jp/3d4070/ffffff/70x70.png?css=%7B%22border-radius%22%3A%2215px%22%7D";
 
 	return (
-		<div className="menuItem drink">
+		<button className="menuItem drink">
 			<img src={imageUrl} />
 			<div className={`content ${!drink.available && "disabled"}`}>
 				<h3>{drink.name}</h3>
 				<span> {drink.description} </span>
 				<span> {drink.label} </span>
 			</div>
-		</div>
+		</button>
 	);
 };
--- src/app/components/MenuItemFood.tsx
+++ src/app/components/MenuItemFood.tsx
@@ -14,13 +14,13 @@ export const MenuItemFood: React.FC<MenuItemFoodProps> = ({ dish }) => {
 		"https://placehold.jp/3d4070/ffffff/70x70.png?css=%7B%22border-radius%22%3A%2215px%22%7D";
 
 	return (
-		<div className="menuItem food">
+		<button className="menuItem food" tabIndex={0}>
 			<img src={imageUrl} />
 			<div className={`content ${!dish.available && "disabled"}`}>
 				<h3>{dish.name}</h3>
 				<span> {dish.description} </span>
 				<span> {dish.label} </span>
 			</div>
-		</div>
+		</button>
 	);
 };
