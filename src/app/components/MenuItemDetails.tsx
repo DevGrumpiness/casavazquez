ca7c82eb0998bf377bb40c4e5bc2fc46a597c16f
DevGrumpiness <b.vazquez@outlook.de>
2023-12-24 18:54:39 +0100

    add back link to detail page

--- src/app/components/MenuItemDetails.tsx
+++ src/app/components/MenuItemDetails.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { TMenuItemDrink, TMenuItemFood } from "../../interfaces/menuItem";
 
 interface MenuItemDetailsProps {
@@ -9,10 +10,16 @@ const MenuItemDetail: React.FC<MenuItemDetailsProps> = ({ item }) => {
 		<p>Item not found</p>;
 	}
 
+	const goBack = () => {
+		window.history.back();
+	};
+
 	return (
 		<div>
 			<h1>Menu Item Details for: </h1>
 			<p>{item.name}</p>
+
+			<button onClick={goBack}>Back to Menu</button>
 		</div>
 	);
 };
