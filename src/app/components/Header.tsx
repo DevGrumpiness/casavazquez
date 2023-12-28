e073080e19de950bb781cf7d4308df3d9e7b310f
DevGrumpiness <b.vazquez@outlook.de>
2023-12-28 18:10:06 +0100

    add logo to header, linked to landingpage

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,21 @@
 import "./Header.scss";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Header: React.FC = () => {
-	return <header>header</header>;
+	return (
+		<header>
+			<Link href="/">
+				<Image
+					src="/media/logo.png"
+					alt="logo"
+					width={40}
+					height={40}
+				/>
+			</Link>
+		</header>
+	);
 };
 
 export default Header;
