aa72a783b2a352464da71f87194cb4a59fdaa482
DevGrumpiness <b.vazquez@outlook.de>
2023-12-28 18:00:59 +0100

    add navigation icons for footer nav

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,16 +1,27 @@
 import "./Footer.scss";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
 	return (
 		<footer>
 			<div className="nav-link-contaienr">
 				<Link className="nav-link" href="/food">
-					Hungry
+					<Image
+						src="/media/food_icon.png"
+						alt="food"
+						width={50}
+						height={50}
+					/>
 				</Link>
 				<Link className="nav-link" href="/drinks">
-					Thirsty
+					<Image
+						src="/media/drinks_icon.png"
+						alt="food"
+						width={50}
+						height={50}
+					/>
 				</Link>
 			</div>
 		</footer>
