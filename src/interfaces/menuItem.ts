7714fe98a4232e917659248315b3811050c0f2d2
DevGrumpiness <b.vazquez@outlook.de>
2023-12-30 20:58:45 +0100

    add DinkSubType to interface

--- src/interfaces/menuItem.ts
+++ src/interfaces/menuItem.ts
@@ -12,12 +12,15 @@ interface MenuItem {
 
 type DrinkSize = "standard" | "small" | "medium" | "large"; // Drinks that come only in one size should be "standard"
 
+type DrinkSubType = "softdrink" | "beer" | "cocktail" | "hot";
+
 export interface TMenuItemFood extends MenuItem {
 	label: "veggie" | "vegan" | null;
 }
 
 export interface TMenuItemDrink extends MenuItem {
 	label: "alcoholic" | "hot" | null;
+	subtype?: DrinkSubType;
 	availableSizes: DrinkSize[];
 }
 
