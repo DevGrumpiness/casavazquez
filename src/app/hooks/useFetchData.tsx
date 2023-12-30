16e49ef2bc5f72d45136054ff2fca5d88ef86c29
DevGrumpiness <b.vazquez@outlook.de>
2023-12-30 17:09:14 +0100

    optimize useFetchData hook

--- src/app/hooks/useFetchData.tsx
+++ src/app/hooks/useFetchData.tsx
@@ -1,25 +1,29 @@
 import { useState, useEffect } from "react";
 
-type TRequestStatus = "idle" | "loading" | "success" | "error";
-type TResponse<T> = {
-	data: T | null;
-	status: TRequestStatus;
-	errorMessage?: string | null;
-};
+type RequestStatus = "idle" | "loading" | "success" | "error";
 
-export const useFetchData = <T,>(url: string): TResponse<T> => {
+interface Response<T> {
+	data: T | null;
+	status: RequestStatus;
+	errorMessage?: string | null;
+}
+
+export const useFetchData = <T,>(url: string): Response<T> => {
 	const [data, setData] = useState<T | null>(null);
-	const [status, setStatus] = useState<TRequestStatus>("idle");
+	const [status, setStatus] = useState<RequestStatus>("idle");
 	const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			setStatus("loading");
+
 			try {
 				const response = await fetch(url);
+
 				if (!response.ok) {
-					throw new Error("Something went wrong");
+					throw new Error("Something went wrong!");
 				}
+
 				const jsonData = await response.json();
 				setData(jsonData);
 				setStatus("success");
@@ -30,7 +34,7 @@ export const useFetchData = <T,>(url: string): TResponse<T> => {
 		};
 
 		fetchData();
-	}, []);
+	}, [url]);
 
 	return { data, status, errorMessage };
 };
