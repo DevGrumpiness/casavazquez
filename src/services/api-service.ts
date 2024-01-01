adc84b6a77ebd26f17f3f55b8b24103a66ee10d1
DevGrumpiness <b.vazquez@outlook.de>
2024-01-01 17:45:20 +0100

    improve api-service.ts to throw an error if supabase url or key are undknown, rather than just using optional chaining to satisfy typescript

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -3,4 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseApiKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
 
-export const supabase = createClient(supabaseUrl ?? "", supabaseApiKey ?? "");
+if (!supabaseUrl) {
+	throw new Error("Missing Supabase URL");
+}
+if (!supabaseApiKey) {
+	throw new Error("Missing Supabase API key");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseApiKey);
