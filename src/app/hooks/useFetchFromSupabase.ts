ab05a64ba84c4c5ff76c15d061e5b83558e25466
DevGrumpiness <b.vazquez@outlook.de>
2024-01-02 19:13:50 +0100

    remove search param from useFetchFromSupabase hook

--- src/app/hooks/useFetchFromSupabase.ts
+++ src/app/hooks/useFetchFromSupabase.ts
@@ -20,6 +20,14 @@ export const useFetchFromSupabase = <T>(tableName: string): Response<T> => {
 			setStatus("loading");
 			try {
 				const response = await supabase.from(tableName).select();
+				const { data, error } = await supabase.storage
+					.from("images")
+					.list("", {
+						limit: 100,
+						offset: 0,
+						sortBy: { column: "name", order: "asc" },
+					});
+				console.log(data);
 				if (response.error) {
 					throw response.error;
 				}
