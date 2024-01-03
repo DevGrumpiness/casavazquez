4c54a74c0601b22e1db7773404fcf1f7e07e3963
DevGrumpiness <b.vazquez@outlook.de>
2024-01-03 19:31:30 +0100

    update next.config.js to allow images from supabase

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		domains: ["eajjoxnhxrvluvseqmud.supabase.co", "*.supabase.co"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
