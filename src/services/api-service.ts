e77b86a8617aa1711667df54ef1961591c0478cd
DevGrumpiness <b.vazquez@outlook.de>
2024-01-03 19:11:59 +0100

    add a functon to fetch from a supabase bucket to api-service

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -13,3 +13,12 @@ if (!supabaseApiKey) {
 export const supabase = createClient(supabaseUrl, supabaseApiKey, {
 	db: { schema: "public" },
 });
+
+export const fetchFromSupabaseBucket = async (bucketName: string) => {
+	const { data, error } = await supabase.storage.from("images").list("", {
+		limit: 100,
+		offset: 0,
+		sortBy: { column: "name", order: "asc" },
+	});
+	console.log(data);
+};
