56f3e704dd05c3281301289dc0863c5a5f1b5ea6
DevGrumpiness <b.vazquez@outlook.de>
2024-01-04 19:46:01 +0100

    refactor listItem component

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -45,7 +45,14 @@ export const ListItem: React.FC<ListItemProps> = ({ listItem, imageUrl }) => {
 							})}
 						</span>
 					</div>
-					<span className="shortDescription"> {listItem.shortDescription} </span>
+					<span className="shortDescription">
+						{listItem.shortDescription}
+						<div className={`expand ${isDetailsOpen ? "hidden" : ""}`} onClick={handleHeaderClick}>
+							<svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
+								<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+							</svg>
+						</div>{" "}
+					</span>
 					<div className={`listItem-details ${!isDetailsOpen ? "hidden" : ""}`}>
 						<hr></hr>
 						{listItem.longDescription}
@@ -57,14 +64,9 @@ export const ListItem: React.FC<ListItemProps> = ({ listItem, imageUrl }) => {
 					{listItem.labels && listItem.labels.map((label) => <span key={label}>{label}</span>)}
 				</div>
 
-				<div className={`close ${!isDetailsOpen && "hidden"}`} onClick={handleCloseIconClick}>
+				<div className={`close ${!isDetailsOpen ? "hidden" : ""}`} onClick={handleCloseIconClick}>
 					<svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
 						<path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z" />
-					</svg>
-				</div>
-				<div className={`expand ${isDetailsOpen && "hidden"}`} onClick={handleHeaderClick}>
-					<svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
-						<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
 					</svg>
 				</div>
 			</div>
