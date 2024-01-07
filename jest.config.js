41a0921e7b9d6616ac3903a9e909be0e1e81a65e
DevGrumpiness <b.vazquez@outlook.de>
2024-01-07 17:33:10 +0100

    update jest.config.js for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-	testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+	preset: "ts-jest",
+	testEnvironment: "jsdom",
 	setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
 	transform: {
-		"^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+		"^.+\\.tsx?$": "ts-jest",
 	},
-	moduleNameMapper: {
-		"\\.(css|less|scss|sass)$": "identity-obj-proxy",
+	globals: {
+		"ts-jest": {
+			tsconfig: "tsconfig.json",
+		},
 	},
 };
