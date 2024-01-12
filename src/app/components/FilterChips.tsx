2c0cbf27584659dac3d0dc51a549aef84e9a7859
DevGrumpiness <b.vazquez@outlook.de>
2024-01-12 18:13:45 +0100

    style filterchips

--- src/app/components/FilterChips.tsx
+++ src/app/components/FilterChips.tsx
@@ -1,16 +1,18 @@
 import "./FilterChips.scss";
 import React from "react";
 
-const mockFilters = ["Alkoholisch", "Bier", "Cocktails", "Heiß"];
+const mockFilters = ["Alkoholisch", "Bier", "Cocktails", "Heiß", "Softdrinks", "Sommerdrinks", "Angebote"];
 
 const FilterChips: React.FC = () => {
 	return (
-		<div className="filterChips">
-			{mockFilters.map((filter, index) => (
-				<div key={index} className="chip">
-					{filter}
-				</div>
-			))}
+		<div className="scrollHint">
+			<div className="filterChips">
+				{mockFilters.map((filter, index) => (
+					<div key={index} className="chip">
+						{filter}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
