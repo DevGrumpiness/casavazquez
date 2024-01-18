f152fee1a873cffc2fdedd388fa549cb71867f21
DevGrumpiness <b.vazquez@outlook.de>
2024-01-18 19:27:32 +0100

    add unselect functionality to filter chips

--- src/app/components/FilterChips.tsx
+++ src/app/components/FilterChips.tsx
@@ -11,23 +11,30 @@ interface FilterChipsProps {
 const FilterChips: React.FC<FilterChipsProps> = ({ selectedFilters, setSelectedFilters }) => {
 	const handleChipClick = (filter: string) => {
 		if (selectedFilters.includes(filter)) {
-			setSelectedFilters(selectedFilters.filter(f => f !== filter));
+			setSelectedFilters(selectedFilters.filter((f) => f !== filter));
 		} else {
 			setSelectedFilters([...selectedFilters, filter]);
 		}
 	};
 
+	const unselect = (filter: string) => () => {
+		setSelectedFilters(selectedFilters.filter((f) => f !== filter));
+	};
+
 	return (
 		<div className="scrollHint">
 			<div className="filterChips">
-				{mockFilters.map((filter, index) => (
-					<div key={index}
-						className={selectedFilters.includes(filter) ? "chip selected" : "chip"}
-						onClick={() => handleChipClick(filter as string)}
-					>
-						{filter}
-					</div>
-				))}
+				{mockFilters.map((filter, index) => {
+					const selected = selectedFilters.includes(filter);
+					return (
+						<div key={index} onClick={() => handleChipClick(filter)}>
+							<div className={selected ? "chip selected" : "chip"}>
+								{filter}
+								<span className={`closeIcon ${!selected ? 'hide' : ''}`} onClick={unselect(filter)} >X</span>
+							</div>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
