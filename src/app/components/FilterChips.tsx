cd7fee82b1fb4eb37a8eee5169d5e988ea8bbae6
DevGrumpiness <b.vazquez@outlook.de>
2024-01-21 20:43:41 +0100

    group active filters at front

--- src/app/components/FilterChips.tsx
+++ src/app/components/FilterChips.tsx
@@ -24,17 +24,21 @@ const FilterChips: React.FC<FilterChipsProps> = ({ selectedFilters, setSelectedF
 	return (
 		<div className="scrollHint">
 			<div className="filterChips">
-				{mockFilters.map((filter, index) => {
-					const selected = selectedFilters.includes(filter);
-					return (
-						<div key={index} onClick={() => handleChipClick(filter)}>
-							<div className={selected ? "chip selected" : "chip"}>
-								<span className="filterLabel">{filter}</span>
-								<span className={`closeIcon ${!selected ? 'hide' : ''}`} onClick={unselect(filter)} >X</span>
+				{mockFilters
+					.sort((a, b) => (selectedFilters.includes(b) ? 1 : -1))
+					.map((filter, index) => {
+						const selected = selectedFilters.includes(filter);
+						return (
+							<div key={index} onClick={() => handleChipClick(filter)}>
+								<div className={selected ? "chip selected" : "chip"}>
+									<span className="filterLabel">{filter}</span>
+									<span className={`closeIcon ${!selected ? "hide" : ""}`} onClick={unselect(filter)}>
+										X
+									</span>
+								</div>
 							</div>
-						</div>
-					);
-				})}
+						);
+					})}
 			</div>
 		</div>
 	);
