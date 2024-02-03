412800fc54ec1e0681af3d197db31820b51e35a5
DevGrumpiness <b.vazquez@outlook.de>
2024-02-03 18:58:19 +0100

    set "use client" in admin page

--- src/app/(routes)/admin/page.tsx
+++ src/app/(routes)/admin/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { TMenuItemDrink } from '../../../interfaces/menuItem';
 import { setDrink, updateDrink } from '../../../services/api-service';
