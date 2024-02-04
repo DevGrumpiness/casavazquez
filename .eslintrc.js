951049f8c16ce8095255bd7492ff58c9133a24ab
DevGrumpiness <b.vazquez@outlook.de>
2024-02-04 18:37:31 +0100

    update eslint to allow mixed spaces and tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
         "react/react-in-jsx-scope": "off",
         "react/jsx-uses-react": "off",
         "react/prop-types": "off",
-        "@typescript-eslint/no-unused-vars": ["off", { varsIgnorePattern: "warn" }],
+		"@typescript-eslint/no-unused-vars": ["off", { varsIgnorePattern: "warn" }],
+		"no-mixed-spaces-and-tabe": "off",
     },
 };
