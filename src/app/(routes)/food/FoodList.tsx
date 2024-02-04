4fe7a2a569b96c877360434579bf072c4937ce39
DevGrumpiness <b.vazquez@outlook.de>
2024-02-04 18:32:43 +0100

    add filter function to FoodList component

--- src/app/(routes)/food/FoodList.tsx
+++ src/app/(routes)/food/FoodList.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { MenuContext } from "../../context/MenuContext";
-import { TMenuItemFood } from "../../../interfaces/menuItem";
+import { TMenuItemDrink, TMenuItemFood } from "../../../interfaces/menuItem";
 import { ListItem } from "../../components/ListItem";
 import { getImageByNameFromBucket } from "../../../services/api-service";
+import FilterChips from "../../components/FilterChips";
+import _ from "lodash";
+
 
 const FoodList: React.FC = () => {
 	const [dishes, setDishes] = useState<TMenuItemFood[]>([]);
+	const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
 	const menuContext = React.useContext(MenuContext);
+
+	const filters = ["Fleisch", "Salat", "Vegetarisch", "Vegan", "Dessert", "Snack"];
+
 
 	if (!menuContext) {
 		return <p>menuContext not found</p>;
@@ -20,8 +27,19 @@ const FoodList: React.FC = () => {
 		}
 	}, [foddData]);
 
+	const filteredDishes = _.filter(dishes, (dish: TMenuItemFood) =>
+		selectedFilters.every((filter) =>
+			dish.label
+				?.toLowerCase()
+				.split(",")
+				.map((label) => label.trim())
+				.includes(filter.toLowerCase())
+		)
+	)
+
 	return (
 		<div className="foodList">
+			<FilterChips filters={filters} selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} />
 			<div className="menuItems">
 				{dishes &&
 					dishes.length > 0 &&
