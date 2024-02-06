7a32387da501c6ae61961134ac513f5ee0c2b1d6
DevGrumpiness <b.vazquez@outlook.de>
2024-02-06 19:41:05 +0100

    add nethods to cart context

--- src/app/context/CartContext.tsx
+++ src/app/context/CartContext.tsx
@@ -1,29 +1,46 @@
-import React, { ReactNode } from 'react';
-import { TMenuItemDrink, TMenuItemFood } from '../../interfaces/menuItem';
+import React, { ReactNode } from "react";
+import { TMenuItemDrink, TMenuItemFood } from "../../interfaces/menuItem";
 
 type CartItem = TMenuItemFood | TMenuItemDrink;
 
 type CartContextValue = {
-    cart: CartItem[];
-    addToCart: (item: CartItem) => void;
+	cart: CartItem[];
+	addToCart: (item: CartItem) => void;
+	removeFromCart: (item: CartItem) => void;
 };
 
-export const CartContext = React.createContext<CartContextValue | undefined>(undefined);
+const defaultContextValue: CartContextValue = {
+	cart: [],
+	addToCart: () => {},
+	removeFromCart: () => {},
+};
+
+export const CartContext = React.createContext<CartContextValue>(defaultContextValue);
 
 interface CartProviderProps {
-    children: ReactNode;
+	children: React.ReactNode;
 }
 
 export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
-    const [cart, setCart] = React.useState<CartItem[]>([]);
+	const [cart, setCart] = React.useState<CartItem[]>([]);
 
-    const addToCart = (item: CartItem) => {
-        setCart(prevCart => [...prevCart, item]);
+	const addToCart = (item: CartItem) => {
+		console.log("addToCart", item);
+		setCart((prevCart) => [...prevCart, item]);
     };
-
-    return (
-        <CartContext.Provider value={{ cart, addToCart }}>
-            {children}
-        </CartContext.Provider>
-    );
+    
+    const removeFromCart = (item: CartItem) => {
+        console.log("removeFromCart", item);
+        setCart((prevCart) => {
+            const index = prevCart.findIndex((cartItem) => cartItem.id === item.id);
+            if (index !== -1) {
+                const newCart = [...prevCart];
+                newCart.splice(index, 1);
+                return newCart;
+            }
+            return prevCart;
+        });
+    };
+    
+	return <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>{children}</CartContext.Provider>;
 };
