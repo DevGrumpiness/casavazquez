332b1b72a83b2cbac8a85ab08fbd1ab8d38dbdb4
DevGrumpiness <b.vazquez@outlook.de>
2024-03-01 19:09:26 +0100

    add tip calculator to beer mat

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,16 +1,23 @@
 import "./Cart.scss";
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import { AddButton } from "./AddButton";
 import { RemoveButton } from "./RemoveButton";
 
 export const Cart: React.FC = () => {
 	const { cart } = useContext(CartContext);
+	const [tipSum, setTipSum] = useState(0);
 
 	const variantTotals = cart.map((item) => item.price * item.quantity);
 
 	const cartTotal = variantTotals.reduce((total, variantTotal) => total + variantTotal, 0);
+
+	const handleTipChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value;
+		const tip = (cartTotal / 100) * parseFloat(value);
+		setTipSum(tip);
+	};
 
 	const renderQuantity = (quantity: number, variant: string | null) => {
 		if (variant === "0,5l" || variant === "0.5l") {
@@ -33,14 +40,25 @@ export const Cart: React.FC = () => {
 						<p>{`${item.name} (${item.variant} - ${item.price} €)`}</p>
 						<div className="count">
 							{renderQuantity(item.quantity, item.variant)}
-							<span>{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(item.price * item.quantity)}</span>
+							<span>
+								{new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(
+									item.price * item.quantity
+								)}
+							</span>
 						</div>
 						<div className="modification_container">
-						<RemoveButton item={item} variant={item.variant} />
-						<AddButton item={item} variant={item.variant} price={item.price} />
+							<RemoveButton item={item} variant={item.variant} />
+							<AddButton item={item} variant={item.variant} price={item.price} />
 						</div>
 					</div>
 				))}
+				<div className="tipSuggestions">
+					<span>
+						Tip:&nbsp;
+						<input className="tipp_input" onChange={handleTipChange} type="number" placeholder="10" />%
+					</span>
+					<div className="tipCalculation">{tipSum.toFixed(2)}</div>
+				</div>
 				<h3>
 					________________
 					<br />
