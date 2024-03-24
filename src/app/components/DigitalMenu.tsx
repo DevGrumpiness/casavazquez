04f2f3939890b55628e4127979ec7841016c1c80
DevGrumpiness <b.vazquez@outlook.de>
2024-03-24 13:53:28 +0100

    add legals

--- src/app/components/DigitalMenu.tsx
+++ src/app/components/DigitalMenu.tsx
@@ -71,15 +71,109 @@ export const DigitalMenu: React.FC = () => {
 						</p>
 					</div>
 				</div>
+				<div className="legals-container">
+					<div className="legals-generall">
+						<p><b>⚠️Hinweis⚠️</b></p>
+						<p>
+							Alle Angaben sind freiwillig und ohne Gewähr. <br />
+							Dies ist eine private Website. Sie wird hobbymäßig entwickelt. <br />
+							Bei Abweichungen gelten immer die Angaben auf der offiziellen Karte/Website. <br />
+							Die offizielle Karte findet sich unter{" "}
+							<a href="http://www.tomundpolly.de/speisekarte.pdf" target="_blank">
+								www.tomundpolly.de/speisekarte.pdf
+							</a>
+							.
+							<br />
+							<br />
+						</p>
+					</div>
+					<div className="legals-haftungsausschluss">
+							<p><b>Haftungsausschluss</b>:</p>
+						<p>
+							Die Informationen auf dieser Website dienen nur zu Informationszwecken und stellen keine
+							rechtliche, finanzielle oder professionelle Beratung dar. Obwohl wir uns bemühen, genaue und
+							aktuelle Informationen bereitzustellen, können wir nicht garantieren, dass alle
+							Informationen korrekt und vollständig sind. Die Nutzung der Informationen auf dieser Website
+							erfolgt auf eigenes Risiko.
+						</p>
+						<p>
+							{" "}
+							Wir übernehmen keine Haftung für Schäden oder Verluste, die durch die Nutzung der auf dieser
+							Website bereitgestellten Informationen oder durch Handlungen, die aufgrund dieser
+							Informationen unternommen werden, entstehen.{" "}
+						</p>
+						<p>
+							Diese Website kann Links zu externen Websites Dritter enthalten, auf deren Inhalte wir
+							keinen Einfluss haben. Für die Inhalte externer Websites übernehmen wir keine Verantwortung
+							oder Haftung. Die Nutzung externer Links erfolgt auf eigenes Risiko. Bitte konsultieren Sie
+							einen qualifizierten Fachmann oder Anwalt für spezifische rechtliche, finanzielle oder
+							berufliche Beratung.
+						</p>
+					</div>
+					<div className="legals-assets">
+							<p><b>Bilder</b>:</p>
+						<p>Auf dieser Website werden freie Bilder von der Plattfor pixabay verwendet.</p>
+						Credits für verwendete Bilder (Links zu den pixaby Seiten der Bildrechtehaber):
+						<br />
+						<br />
+						<a href="https://pixabay.com/de/users/quadronet_webdesign-1555018/">quadronet_webdesign</a>
+						<br />
+						<a href="https://pixabay.com/de/users/mim326-1771256/">mim326</a>
+						<br />
+						<a href="https://pixabay.com/de/users/openclipart-vectors-30363/">openclipart-vectors</a>
+						<br />
+						<a href="https://pixabay.com/de/users/alles-2597842/">alles</a>
+						<br />
+						<a href="https://pixabay.com/de/users/clker-free-vector-images-3736/">
+							clker-free-vector-images
+						</a>
+						<br />
+						<a href="https://pixabay.com/de/users/roly96-17414152/">roly96</a>
+						<br />
+						<a href="https://pixabay.com/de/users/billithecat-7996303/">billithecat</a>
+						<br />
+						<a href="https://pixabay.com/de/users/deeznutz1-3086161/">deeznutz1</a>
+						<br />
+						<a href="https://pixabay.com/de/users/distelapparath-2726923/">distelapparath</a>
+						<br />
+						<a href="https://pixabay.com/de/users/furbymama-5146222/">furbymama</a>
+						<br />
+						<a href="https://pixabay.com/de/users/satheeshsankaran-11196627/">satheeshsankaran</a>
+						<br />
+						<a href="https://pixabay.com/de/users/gerippefu-3153001/">gerippefu</a>
+						<br />
+						<a href="https://pixabay.com/de/users/midicomp-2657342/">midicomp</a>
+						<br />
+						<a href="https://pixabay.com/de/users/openclipart-vectors-30363/">openclipart-vectors</a>
+						<br />
+						<a href="https://pixabay.com/de/users/ri_ya-12911237/">ri_ya</a>
+						<br />
+						<a href="https://pixabay.com/de/users/wbe1951-472794/">wbe1951</a>
+						<br />
+						<a href="https://pixabay.com/de/users/vika_glitter-6314823/">vika_glitter</a>
+						<br />
+						<a href="https://pixabay.com/de/users/intelligentvisualdesing-33770741/">
+							intelligentvisualdesing
+						</a>
+						<br />
+						<a href="https://pixabay.com/de/users/thedigitalartist-202249/">thedigitalartist</a>
+						<br />
+						<a href="https://pixabay.com/de/users/openclipart-vectors-30363/">openclipart-vectors</a>
+						<br />
+						<a href="https://pixabay.com/de/users/designdrawartes-8580564/">designdrawartes</a>
+						<br />
+						<a href="https://pixabay.com/de/users/elisariva-1348268/">elisariva</a>
+						<br />
+						<br />
+					</div>
+					<div className="legals-impressum">
+						<b>Impressum:</b>
+						<p>José Guerrero Vazquez</p>
+						<p>jbmj@outlook.de</p>
+					</div>
+
+				</div>
 			</div>
-			<footer>
-				ℹ️ Hinweise:
-				<p>	
-					Dies ist eine private Website.
-					Bei Abweichungen gelten immer die Angaben auf der offiziellen Karte/Website. <br />
-					Die offizielle Karte findet sich unter http://www.tomundpolly.de/speisekarte.pdf 
-				</p>
-			</footer>
 		</>
 	);
 };
