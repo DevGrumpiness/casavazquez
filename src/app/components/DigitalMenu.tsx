c60bd85e343b77b51b4694e1d42452bf34d75f9f
DevGrumpiness <b.vazquez@outlook.de>
2024-03-24 11:38:02 +0100

    legals hint

--- src/app/components/DigitalMenu.tsx
+++ src/app/components/DigitalMenu.tsx
@@ -27,50 +27,59 @@ export const DigitalMenu: React.FC = () => {
 	console.log("news", news);
 
 	return (
-		<div className="digitalMenu">
-			<div>
-				<h3>Aktuelles:</h3>
-				<div className="mobileOptimizedHint">⚠️ Fürs Handy optimiert ⚠️</div>
-				{news.length > 0 ? renderNews() : null}
-			</div>
-
-				<hr></hr>
-			<div className="hours-container">
-				<hr></hr>
-				<h3>Aktuelle Öffnungszeiten</h3>
-				<div className="hours">
-					<p>
-						<span>Mo-Do</span>
-						<span>15:00-01:00</span>
-					</p>
-					<p>
-						<span>Fr</span>
-						<span>15:00-02:00</span>
-					</p>
-					<p>
-						<span>Sa</span>
-						<span>12:00-02:00</span>
-					</p>
-					<p>
-						<span>So</span>
-						<span>15:00-00:00</span>
-					</p>
-					<h3>Küche</h3>
-					<p>
-						<span>Mo-Fr</span>
-						<span>18:00-22:00</span>
-					</p>
-					<p>
-						<span>Sa</span>
-						<span>12:00-14:00</span>
-						<span>15:00-22:00</span>
-					</p>
-					<p>
-						<span>So</span>
-						<span>16:00-21:00</span>
-					</p>
+		<>
+			<div className="digitalMenu">
+				<div>
+					<h3>Aktuelles:</h3>
+					<div className="mobileOptimizedHint">⚠️ Fürs Handy optimiert ⚠️</div>
+					{news.length > 0 ? renderNews() : null}
+				</div>
+				<br />
+				<div className="hours-container">
+					<hr></hr>
+					<h3>Aktuelle Öffnungszeiten</h3>
+					<div className="hours">
+						<p>
+							<span>Mo-Do</span>
+							<span>15:00-01:00</span>
+						</p>
+						<p>
+							<span>Fr</span>
+							<span>15:00-02:00</span>
+						</p>
+						<p>
+							<span>Sa</span>
+							<span>12:00-02:00</span>
+						</p>
+						<p>
+							<span>So</span>
+							<span>15:00-00:00</span>
+						</p>
+						<h3>Küche</h3>
+						<p>
+							<span>Mo-Fr</span>
+							<span>18:00-22:00</span>
+						</p>
+						<p>
+							<span>Sa</span>
+							<span>12:00-14:00</span>
+							<span>15:00-22:00</span>
+						</p>
+						<p>
+							<span>So</span>
+							<span>16:00-21:00</span>
+						</p>
+					</div>
 				</div>
 			</div>
-		</div>
+			<footer>
+				ℹ️ Hinweise:
+				<p>	
+					Dies ist eine private Website.
+					Bei Abweichungen gelten immer die Angaben auf der offiziellen Karte/Website. <br />
+					Die offizielle Karte findet sich unter http://www.tomundpolly.de/speisekarte.pdf 
+				</p>
+			</footer>
+		</>
 	);
 };
