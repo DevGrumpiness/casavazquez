819274f86d58f3b0379ec1b2d24de9df05d4851a
DevGrumpiness <b.vazquez@outlook.de>
2025-06-28 14:21:17 +0200

    Add FastAPI backend with MCP support

---
+++ casavazquez/backend/app/main.py
@@ -0,0 +1,6 @@
+﻿from fastapi import FastAPI  # https://fastapi.tiangolo.com/tutorial/first-steps/
+from app.api import vinos
+
+app = FastAPI(title="Casa Vazquez API")  # Titel erscheint in Swagger UI
+
+app.include_router(vinos.router)  #  https://fastapi.tiangolo.com/tutorial/bigger-applications/#include-the-router
